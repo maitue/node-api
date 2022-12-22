d632116569e85e3e631c5a66f03021ff6468a77c
maitue <44012114+maitue@users.noreply.github.com>
2022-12-22 16:23:11 +0700

    change column question

---
+++ database/migrations/2022_12_22_092003_change_column_name_question.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('correct_answer');
+            $table->renameColumn('question_name', 'question');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('correct_answer');
+            $table->renameColumn('question', 'question_name');
+        });
+    }
+};
